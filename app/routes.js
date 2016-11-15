ebaf735cef2664d967516dea4c0ab9a558384db4
anisha1234 <anisha.swain@yahoo.in>
2016-11-15 17:26:23 +0530

    First commit by Anisha

---
+++ app/routes.js
@@ -0,0 +1,31 @@
+//require experess
+var experess = require('express');
+var path = require('path');
+
+//create our router object
+var router = experess.Router();
+
+//export our router
+module.exports = router;
+
+
+//route for our home
+router.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'../index.html'));
+});
+
+//route for our about page
+router.get('/about',function(req,res){
+	var users = [
+	{name:}
+	];
+	res.sendFile(path.join(__dirname,'../about.html'));	
+});
+
+//route for contatct i.e. one get page and one post
+router.get('/contact',function(req,res){
+	res.sendFile(path.join(__dirname,'../contact.html'));
+});
+router.post('/contact',function(req,res){
+	
+});
---
+++ server.js
@@ -0,0 +1,44 @@
+/*import htp module
+var http = require('http');
+
+
+//handle sending requests and returning responses
+function handleRequest(req,res)
+{
+
+//return strings
+res.end("hello world");
+}
+//create the server
+var server = http.createServer(handleRequest);
+
+//start server and listen on port x
+server.listeng('8000',function(){
+	console.log("listenin to port 8000");
+});*/
+
+
+
+/////////////////////  EXPRESS //////////////////
+//require our dependencies
+var express = require('express');
+var app = express();
+var port = 8080;
+
+//route our app
+var router = require('./app/routes');
+
+
+
+//set static files(css or js or imgs)
+app.use(express.static(__dirname + "/public"));
+
+
+//express middlewire which have access to all our routes
+app.use('/',router);
+
+//start your server
+app.listen(port,function(){
+	console.log('app started')
+});
+
